5f358cb6f7ec4b0c9233b47b76c6c6b8ad84bd01
Neha <nehapansare007@gmail.com>
2025-03-30 21:17:27 +0530

    images added

--- Client/my-project/src/assets/Home/Home.jsx
+++ Client/my-project/src/assets/Home/Home.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
+import homeImage from "../../assets/img/image1.jpg"; // Adjust the path as needed
 
 function Home() {
   return (
-    <div>
-       <h1 class="text-7xl font-bold underline">
-    Hello Neha!
-  </h1>
+    <div className="flex justify-center items-center bg-gray-100">
+      <img src={homeImage} alt="Home" className="w-[1450px] h-[900px] object-contain" />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
