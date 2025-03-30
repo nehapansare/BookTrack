4fa5adaf42c0203563b88962d26e785c541e9872
Neha <nehapansare007@gmail.com>
2025-03-30 17:56:11 +0530

    route added

--- Client/my-project/src/main.jsx
+++ Client/my-project/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Home from './assets/Home/Home';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  
+  
+  
+]);
+
+root.render(
+  <RouterProvider router={router} />
+);
