e6f5a9baf96e0b02b9fde2c573d2a912337384d7
Neha <nehapansare007@gmail.com>
2025-03-31 12:25:35 +0530

    genres added

--- Server/Modules/Book.js
+++ Server/Modules/Book.js
@@ -1,15 +1,16 @@
+import { model, Schema } from "mongoose";
 
-import {model,Schema} from 'mongoose'
 const bookSchema = new Schema({
   title: String,
-  author:   String,  
-  cover:   String ,
-  language:  String,  
-  rating:   Number,
+  author: String,
+  cover: String,
+  language: String,
+  rating: Number,
   year: Number,
   availableCopies: Number,
-  totalCopies: Number
-},{timestamps:true});
+  totalCopies: Number,
+  genres: [String] // <-- Added genres as an array of strings
+}, { timestamps: true });
 
 const Book = model("Book", bookSchema);
 export default Book;
