6633f9acda9764a828b024599756816b60167cb3
Neha <nehapansare007@gmail.com>
2025-03-31 19:07:34 +0530

    Navbar contact,about updated

--- Client/my-project/src/Component/Navbar/Navbar.jsx
+++ Client/my-project/src/Component/Navbar/Navbar.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className="flex justify-between items-center p-5 bg-[#476442] text-white">
-      <div>
-        <img src="/logo.png" alt="Logo" className="h-10" />
-      </div>
-      <div className="flex gap-5 text-xl">
+     <div className=" font-bold">
+    <span className='text-3xl'>BOOK</span><span className='text-2xl text-[#2D2429]'>CLUB</span>
+</div>
+
+      <div className="flex gap-30 text-xl">
         <a href="/" className="text-white no-underline">Home</a>
         <a href="/bookshelf" className="text-white no-underline">BookShelf</a>
+        <a href="/bookshelf" className="text-white no-underline">Contact </a>
+        <a href="/bookshelf" className="text-white no-underline">About Us</a>
+        
       </div>
       <a href="/login" className="text-white no-underline text-lg">Login</a>
     </nav>
