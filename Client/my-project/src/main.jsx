a3a137424ec2960fc7fbcdd0e4d640d2cd5822a7
Neha <nehapansare007@gmail.com>
2025-03-31 10:53:11 +0530

    main.js modified

--- Client/my-project/src/main.jsx
+++ Client/my-project/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Views/Home/Home';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BookShelf from './Component/BookShelf/BookShelf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,7 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <Home />
   },
-  
+  {
+    path:"/bookshelf",
+    element:<BookShelf/>
+  }
   
   
 ]);
